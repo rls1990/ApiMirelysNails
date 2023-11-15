c8e55c11dfe79bcc1672bf6285fbeac277e4e82e
rls1990 <roilanlauzardosotolongo@gmail.com>
2023-11-15 03:59:03 -0500

    modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -23,13 +23,22 @@ const url = ["https://mirelysnailsadmin.onrender.com"];
 const app = express();
 
 //middleware
-app.use(
+/*app.use(
   cors({
     origin: true,
     url,
     credentials: true,
   })
-);
+);*/
+
+app.use(cors());
+ 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 app.use(morgan("dev"));
 //app.use(express.urlencoded({ extended: true }));
